99bf403d1eb834b3d7f71aab09bc91cd608e1ba5
Pham Van Khang <phamvankhang.tvi@gmail.com>
2026-08-27 16:43:33 +0700

    feat: add AddUserIsActive migration to ApiConfigService

--- src/shared/services/api-config.service.ts
+++ src/shared/services/api-config.service.ts
@@ -18,6 +18,7 @@ import { SeedRelay2ChAcs712Template1788100000000 } from '../../database/migratio
 import { AddRelayCurrentNodeTemplateType1788200000000 } from '../../database/migrations/1788200000000-AddRelayCurrentNodeTemplateType.ts';
 import { AddUnclaimedDevices1788300000000 } from '../../database/migrations/1788300000000-AddUnclaimedDevices.ts';
 import { SeedGatewayTemplate1788400000000 } from '../../database/migrations/1788400000000-SeedGatewayTemplate.ts';
+import { AddUserIsActive1788500000000 } from '../../database/migrations/1788500000000-AddUserIsActive.ts';
 import { UserSubscriber } from '../../entity-subscribers/user-subscriber.ts';
 import { DashboardEntity } from '../../modules/dashboard/dashboard.entity.ts';
 import { DeviceEntity } from '../../modules/device/device.entity.ts';
@@ -127,6 +128,7 @@ export class ApiConfigService {
         AddRelayCurrentNodeTemplateType1788200000000,
         AddUnclaimedDevices1788300000000,
         SeedGatewayTemplate1788400000000,
+        AddUserIsActive1788500000000,
       ],
       dropSchema: this.isTest,
       type: 'postgres',
