2ca1447236435c06ee84c0cf4b44e6e173374305
Pham Van Khang <phamvankhang.tvi@gmail.com>
2026-08-28 00:23:53 +0700

    fix: change version handling to ensure it is treated as a string in Vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig, loadEnv } from 'vite';
 // https://vite.dev/config/
 export default defineConfig( ({mode}) => {
   const env = loadEnv(mode, process.cwd(), "VITE_");
-  const version = Number(env.VITE_APP_VERSION ?? "0.0.0");
+  const version = String(env.VITE_APP_VERSION ?? "0.0.0");
   
   return {
   define: {
