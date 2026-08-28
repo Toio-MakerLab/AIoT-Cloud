64c08c7e5e192df9b7b1633c9eff821d1051618d
Pham Van Khang <phamvankhang.tvi@gmail.com>
2026-08-28 21:26:27 +0700

    fix: update content security policy to include fcmregistrations.googleapis.com

--- src/main.ts
+++ src/main.ts
@@ -35,10 +35,10 @@ export async function bootstrap(): Promise<NestExpressApplication> {
       contentSecurityPolicy: {
         directives: {
           ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-          'connect-src': ["'self'", 'blob:', 'https://cdn.jsdelivr.net', 'https://www.gstatic.com', 'https://firebaseinstallations.googleapis.com', 'https://fcm.googleapis.com', 'https://*.firebaseio.com'],
-          'worker-src': ["'self'", 'blob:', 'https://cdn.jsdelivr.net', 'https://www.gstatic.com', 'https://firebaseinstallations.googleapis.com', 'https://fcm.googleapis.com', 'https://*.firebaseio.com'],
-          'script-src': ["'self'", 'blob:', 'https://cdn.jsdelivr.net', 'https://www.gstatic.com', 'https://firebaseinstallations.googleapis.com', 'https://fcm.googleapis.com', 'https://*.firebaseio.com'],
-          'script-src-elem': ["'self'", 'blob:', 'https://cdn.jsdelivr.net', 'https://www.gstatic.com', 'https://firebaseinstallations.googleapis.com', 'https://fcm.googleapis.com', 'https://*.firebaseio.com'],
+          'connect-src': ["'self'", 'blob:', 'https://cdn.jsdelivr.net', 'https://www.gstatic.com', 'https://firebaseinstallations.googleapis.com', 'https://fcm.googleapis.com', 'https://*.firebaseio.com', 'https://fcmregistrations.googleapis.com'],
+          'worker-src': ["'self'", 'blob:', 'https://cdn.jsdelivr.net', 'https://www.gstatic.com', 'https://firebaseinstallations.googleapis.com', 'https://fcm.googleapis.com', 'https://*.firebaseio.com', 'https://fcmregistrations.googleapis.com'],
+          'script-src': ["'self'", 'blob:', 'https://cdn.jsdelivr.net', 'https://www.gstatic.com', 'https://firebaseinstallations.googleapis.com', 'https://fcm.googleapis.com', 'https://*.firebaseio.com','https://fcmregistrations.googleapis.com'],
+          'script-src-elem': ["'self'", 'blob:', 'https://cdn.jsdelivr.net', 'https://www.gstatic.com', 'https://firebaseinstallations.googleapis.com', 'https://fcm.googleapis.com', 'https://*.firebaseio.com', 'https://fcmregistrations.googleapis.com'],
         },
       },
     }),
