d82e53f08fc85c84b1fb16feaee7a3818a39afa8
Pham Van Khang <phamvankhang.tvi@gmail.com>
2026-08-30 04:55:25 +0700

    feat: add KafkaTopicsInitializer to KafkaModule providers

--- src/modules/kafka/kafka.module.ts
+++ src/modules/kafka/kafka.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { DeviceModule } from '../device/device.module.ts';
 import { KafkaController } from './kafka.controller.ts';
+import { KafkaTopicsInitializer } from './kafka-topics.initializer.ts';
 
 @Module({
   imports: [DeviceModule],
   controllers: [KafkaController],
+  providers: [KafkaTopicsInitializer],
 })
 export class KafkaModule {}
